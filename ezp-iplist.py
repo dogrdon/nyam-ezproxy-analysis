902118568bf08e56332f6704e3d96e93b2c4e08e
droquo <drew.s.gordon@gmail.com>
2014-07-14 17:14:59 -0400

    remove lines that GET unimportant server resources

--- ezp-iplist.py
+++ ezp-iplist.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import re
 from collections import Counter
 from pprint import pprint
 
@@ -19,9 +20,16 @@ def get_ips():
         lines = [line.strip() for line in open(curr_log)]
 
         for line in lines:
-            ip_list.append(line.split('-')[0])
 
-        return ip_list
+            garbage = re.search(r'\/img\/|\/images\/|\/style\/|\/script\/|\/foresee\/\g', line) #not 100% exhaustive but this should remove lines that GET server resources like scripts, images and stylesheets
+
+            if garbage:
+                #print "I'm a garbage line."
+                pass
+            else:
+                ip_list.append(line.split('-')[0])
+
+    return ip_list
     
 
 def count_ips(ip_list):
